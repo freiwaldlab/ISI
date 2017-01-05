a4fb5bbfc07ca5ddb0f54ba47bbe69d1b1bcfca3
David Hildebrand <david@hildebrand.name>
2017-01-04 19:26:59 -0500

    MATLAB version compatibility

--- Stimulator/Vstim code (ISI comp)/Stimulator_master/sync_inputs/configSyncInput.m
+++ Stimulator/Vstim code (ISI comp)/Stimulator_master/sync_inputs/configSyncInput.m
@@ -2,8 +2,15 @@ function configSyncInput
 
 global analogIN
 
-analogIN = analoginput('nidaq','Dev1');
-set(analogIN, 'SampleRate', 10000);
-actualInputRate = get(analogIN, 'SampleRate');
-addchannel(analogIN,[0 1]);
-set(analogIN,'SamplesPerTrigger',inf); 
+%legacy code. no longer supported by matlab. mmf
+% analogIN = analoginput('nidaq','Dev1');
+% set(analogIN, 'SampleRate', 10000);
+% actualInputRate = get(analogIN, 'SampleRate');
+% addchannel(analogIN,[0 1]);
+% set(analogIN,'SamplesPerTrigger',inf);
+
+analogIN = daq.createSession('ni');
+addAnalogInputChannel(analogIN,'Dev1',0:1,'Voltage');
+analogIN.Rate = 5000; %rate is determined by maximum on device/number of channels
+actualInputRate = analogIN.Rate; %to account for rounding done by system
+analogIN.IsContinuous = true;
