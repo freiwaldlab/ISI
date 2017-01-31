c4d4529eba037174bbfd21da4c879f0ea66dd04b
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-01-31 15:44:48 -0500

    readability formatting

--- Stimulator/master/DisplayCode/refreshLooperView.m
+++ Stimulator/master/DisplayCode/refreshLooperView.m
@@ -1,18 +1,15 @@
 function refreshLooperView
-
-global Lstate GUIhandles
-
-set(GUIhandles.looper.repeats,'string',Lstate.reps)
-set(GUIhandles.looper.randomflag,'value',Lstate.rand)
-
-set(GUIhandles.looper.formula,'string',Lstate.formula)
-
-for i = 1:length(Lstate.param)
+    global Lstate GUIhandles
+    Gh = GUIhandles;
     
-    eval(['symhandle = GUIhandles.looper.symbol' num2str(i) ';'])
-    eval(['valhandle = GUIhandles.looper.valvec' num2str(i) ';'])
-
-    set(symhandle,'string',Lstate.param{i}{1})
-    set(valhandle,'string',Lstate.param{i}{2})
-
-end
+    set(Gh.looper.repeats, 'string', Lstate.reps)
+    set(Gh.looper.randomflag, 'value', Lstate.rand)
+    set(Gh.looper.formula, 'string', Lstate.formula)
+    for i = 1:length(Lstate.param)
+        eval(['symhandle = Gh.looper.symbol' num2str(i) ';'])
+        eval(['valhandle = Gh.looper.valvec' num2str(i) ';'])
+        set(symhandle, 'string', Lstate.param{i}{1})
+        set(valhandle, 'string', Lstate.param{i}{2})
+    end
+    
+    GUIhandles = Gh;
