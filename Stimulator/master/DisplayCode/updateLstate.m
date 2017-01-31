d711dc5e77bbe03b772de2adf2ba95cfeb423709
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-01-31 15:15:28 -0500

    added default looper settings

--- Stimulator/master/DisplayCode/updateLstate.m
+++ Stimulator/master/DisplayCode/updateLstate.m
@@ -1,24 +1,31 @@
 function updateLstate
+    global Lstate GUIhandles
+    Gh = GUIhandles;
+    
+    Lstate.reps = str2double(get(Gh.looper.repeats, 'string'));
+    Lstate.rand = get(Gh.looper.randomflag, 'value');
 
-%Ian Nauhaus
+    Ldum{1} = {[get(Gh.looper.symbol1, 'string')] ...
+        [get(Gh.looper.valvec1, 'string')]};
+    Ldum{2} = {[get(Gh.looper.symbol2, 'string')] ...
+        [get(Gh.looper.valvec2, 'string')]};
+    Ldum{3} = {[get(Gh.looper.symbol3, 'string')] ...
+        [get(Gh.looper.valvec3, 'string')]};
+    %Ldum{4} = {[get(Gh.looper.symbol4, 'string')] ...
+    %    [get(Gh.looper.valvec4, 'string')]};
+    %Ldum{5} = {[get(Gh.looper.symbol5, 'string')] ...
+    %    [get(Gh.looper.valvec5, 'string')]};
 
-global Lstate GUIhandles
-
-Lstate.reps = str2num(get(GUIhandles.looper.repeats,'string'));
-Lstate.rand = get(GUIhandles.looper.randomflag,'value');
-
-Ldum{1} = {[get(GUIhandles.looper.symbol1,'string')] [get(GUIhandles.looper.valvec1,'string')]};
-Ldum{2} = {[get(GUIhandles.looper.symbol2,'string')] [get(GUIhandles.looper.valvec2,'string')]};
-Ldum{3} = {[get(GUIhandles.looper.symbol3,'string')] [get(GUIhandles.looper.valvec3,'string')]};
-
-%Get rid of blank rows...
-Lstate.param = cell(1,1);  %initialize
-k = 1;
-for i = 1:length(Ldum)
-    if ~isempty(Ldum{i}{1})
-        Lstate.param{k} = Ldum{i};        
-        k = k+1;
+    % Purge any blank settings
+    Lstate.param = cell(1, 1);
+    k = 1;
+    for i = 1:length(Ldum)
+        if ~isempty(Ldum{i}{1})
+            Lstate.param{k} = Ldum{i};        
+            k = k + 1;
+        end
     end
-end
 
-Lstate.formula = get(GUIhandles.looper.formula,'string');
+    Lstate.formula = get(Gh.looper.formula, 'string');
+    
+    GUIhandles = Gh;
