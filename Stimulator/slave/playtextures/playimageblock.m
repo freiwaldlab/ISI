c858cecff4e3dbd206bac2d0d26e354ef90ec6a5
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-02-11 13:01:13 -0500

    sync high for first half of predelay; sync high and low variables created to prevent confusion

--- Stimulator/slave/playtextures/playimageblock.m
+++ Stimulator/slave/playtextures/playimageblock.m
@@ -2,6 +2,8 @@ function playimageblock
     global Mstate screenPTR screenNum
     global Gtxtr TDim % from makeImageBlockTexture
     global Stxtr % from makeSyncTexture
+    syncHigh = Stxtr(1);
+    syncLow = Stxtr(2);
 
     P = getParamStruct;
     window = screenPTR;
@@ -11,20 +13,6 @@ function playimageblock
     syncWpx = round(resXpxpercm * Mstate.syncSize);
     syncHpx = round(resYpxpercm * Mstate.syncSize);
     
-    % %%% DEBUG XXX ***
-    % % Get the screen numbers
-    % screens = Screen('Screens');
-    % % Draw to the external screen if avaliable
-    % screenNumber = max(screens);
-    % % Open an on screen window
-    % [window, ~] = Screen('OpenWindow', screenNumber, 128, [0 0 500 500]);
-    % Mstate.screenXcm = 5;
-    % Mstate.screenYcm = 5;
-    
-    % % Get the size of the on screen window
-    % [screenXpx, screenYpx] = Screen('WindowSize', window);
-    % % Get the centre coordinate of the window
-    % [screenCenterX, screenCenterY] = RectCenter(windowRect);
     % % Query the frame duration
     % ifi = Screen('GetFlipInterval', window);
 
@@ -48,7 +36,7 @@ function playimageblock
     end
     
     % Calculate the pixel range of the stimulus
-    %   NOTE: Truncating these things to the screen size messes things up.
+    %   NOTE: Truncating to the screen size messes things up.
     imWpx = TDim(1);
     imHpx = TDim(2);
     rngXpx = [(P.x_pos - floor(stimWpx / 2) + 1) ...
@@ -79,20 +67,22 @@ function playimageblock
     end
 
     % Pre-delay
-    % Draw "high" sync state quickly to indicate beginning of new block
-    Screen('DrawTexture', window, Stxtr(1), syncPiece, syncPos);
+    % Draw "high" sync state for first half of pre-delay to indicate 
+    % beginning of new block
+    Screen('DrawTexture', window, syncHigh, syncPiece, syncPos);
     Screen('Flip', window);
+    WaitSecs(P.predelay / 2);
     % Draw "low" sync state during rest of pre-delay
-    Screen('DrawTexture', window, Stxtr(2), syncPiece, syncPos);
+    Screen('DrawTexture', window, syncLow, syncPiece, syncPos);
     Screen('Flip', window);
-    WaitSecs(P.predelay);
+    WaitSecs(P.predelay / 2);
     
     % Play stimulus by drawing pre-generated textures to the screen
     for imn = imList
         % Simultaneously draw "high" sync and image to the screen
         %   Unless otherwise specified PTB will draw the texture 
         %   full size in the center of the screen
-        Screen('DrawTextures', window, [Stxtr(1) Gtxtr(imn)], ...
+        Screen('DrawTextures', window, [syncHigh Gtxtr(imn)], ...
            [syncPiece stimPiece], [syncPos stimPos], [0 P.ori]);
         % Flip to the window
         Screen('Flip', window);
@@ -100,7 +90,7 @@ function playimageblock
         WaitSecs(P.image_duration);
         % Now simultaneously draw "low sync" and blank the screen 
         % with the background
-        Screen('DrawTexture', window, Stxtr(2), syncPiece, syncPos);
+        Screen('DrawTexture', window, syncLow, syncPiece, syncPos);
         % Flip to the window
         Screen('Flip', window);
         % Wait for specified inter-image interval unless this is the 
@@ -113,6 +103,6 @@ function playimageblock
     
     % Post-delay
     % Draw "low" sync state for duration of post-delay block
-    Screen('DrawTexture', window, Stxtr(2), syncPiece, syncPos);
+    Screen('DrawTexture', window, syncLow, syncPiece, syncPos);
     Screen('Flip', window);
     WaitSecs(P.postdelay);
