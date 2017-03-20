889c642293622d7741974121e3d0c3dc1f171595
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-03-20 15:43:17 -0400

    readability and cleanup

--- Stimulator/master/COM_display/waitforDisplayResp.m
+++ Stimulator/master/COM_display/waitforDisplayResp.m
@@ -1,25 +1,22 @@
-function waitforDisplayResp   
+function waitforDisplayResp
+    global DcomState 
 
-global DcomState 
+    comhandle = DcomState.serialPortHandle;
 
-comhandle = DcomState.serialPortHandle;
+    % Clear the buffer
+    n = get(comhandle, 'BytesAvailable');
+    if n > 0
+        fread(comhandle, n); %clear the buffer
+    end
 
-%Clear the buffer
-n = get(comhandle,'BytesAvailable');
-if n > 0
-    fread(comhandle,n); %clear the buffer
-end
+    % Wait...
+    n = 0;  %Need this, or it won't enter next loop (if there were leftover bits)!!!!
+    while n == 0
+        n = get(comhandle, 'BytesAvailable'); %Wait for response
+    end
+    pause(.5) %Hack to finish the read
 
-%Wait...
-n = 0;  %Need this, or it won't enter next loop (if there were leftover bits)!!!!
-while n == 0
-    n = get(comhandle,'BytesAvailable'); %Wait for response
-end
-pause(.5) %Hack to finish the read
-
-
-n = get(comhandle,'BytesAvailable');
-if n > 0
-    fread(comhandle,n); %clear the buffer
-end
-
+    n = get(comhandle, 'BytesAvailable');
+    if n > 0
+        fread(comhandle, n); %clear the buffer
+    end
