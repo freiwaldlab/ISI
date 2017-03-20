64c0c8fdb537072867d1bbf453d009efda8a60e4
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-03-20 14:12:57 -0400

    send confirmation to master after texture made

--- Stimulator/slave/makeTexture.m
+++ Stimulator/slave/makeTexture.m
@@ -1,23 +1,31 @@
 function makeTexture(modID)
-switch modID
-    case 'PG'  %Periodic Grater
-        makeGratingTexture_periodic
-    case 'FG'  %Flash Grater
-        if getParamVal('FourierBit')
-            makeGratingTexture_flash
-        else
-            makeGratingTexture_flashCartesian
-        end
-    case 'RD'  %Raindropper
-        makeRainTexture
-    case 'FN'  %Filtered Noise
-        makeNoiseTexture        
-    case 'MP'  %Mapper
-        %makeMapper  %No need to a make file
-    case 'CM'
-        makeCohMotion
-    case 'IB'
-        makeImageBlockTexture
-    otherwise
-        disp('makeTexture ERROR: Unknown module ID.')
-end
+    global comState
+    
+    makeDone = true;
+    switch modID
+        case 'PG'  %Periodic Grater
+            makeGratingTexture_periodic
+        case 'FG'  %Flash Grater
+            if getParamVal('FourierBit')
+                makeGratingTexture_flash
+            else
+                makeGratingTexture_flashCartesian
+            end
+        case 'RD'  %Raindropper
+            makeRainTexture
+        case 'FN'  %Filtered Noise
+            makeNoiseTexture        
+        case 'MP'  %Mapper
+            %makeMapper  %No need to a make file
+        case 'CM'
+            makeCohMotion
+        case 'IB'
+            makeImageBlockTexture
+        otherwise
+            disp('makeTexture ERROR: Unknown module ID.')
+            makeDone = false;
+    end
+    if makeDone
+        fwrite(comState.serialPortHandle, ...
+            strcat('MT;', modID, ';~'))
+    end
