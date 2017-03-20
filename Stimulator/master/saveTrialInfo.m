c4c9afbd81282be518cbe4c20ff1a03dbf4a732e
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-03-20 15:43:46 -0400

    stim order saving

---
+++ Stimulator/master/saveTrialInfo.m
@@ -0,0 +1,19 @@
+function saveTrialInfo(trialInfo)
+    global Mstate trialno DataPath
+
+    eval(['trialInfo' num2str(trialno) '=trialInfo;'])
+
+    % Make sure destination directory exists and save the analyzer file
+    title = [Mstate.anim '_' sprintf('u%s', Mstate.unit) '_' Mstate.expt];
+    roots = parseString(DataPath, ';');
+    for i = 1:length(roots)
+        dd = [roots{i} filesep];
+        % Check if directory exists in case this is a new animal
+        if(~exist(dd, 'dir'))
+            mkdir(dd);
+        end
+        file_name = [dd filesep title '.analyzer'];
+        disp(['saveTrialInfo: Appending trialInfo to .analyzer file (' ...
+            file_name ').'])
+        save(file_name, ['trialInfo' num2str(trialno)], '-append')
+    end
