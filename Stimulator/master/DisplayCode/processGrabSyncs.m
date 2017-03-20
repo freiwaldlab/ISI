6b8c7958edbdda4bb5b6cd89526fef7ed5ae1f6b
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-03-20 15:19:13 -0400

    readability; cleanup

--- Stimulator/master/DisplayCode/processGrabSyncs.m
+++ Stimulator/master/DisplayCode/processGrabSyncs.m
@@ -1,22 +1,12 @@
-function synctimes = processGrabSyncs(syncwave,Fs)
+function synctimes = processGrabSyncs(syncwave, Fs)
+    % Produces a vector corresponding to the rising edge times of syncwave
+    high = max(syncwave);
+    low = median(syncwave);  % median to avoid negative transients
+    thresh = (high + low) / 2;
 
-%Produces a vector corresponding to the rising edge times of syncwave
-
-high = max(syncwave);
-low = median(syncwave);  %Sometimes there are negative transients
-thresh = (high+low)/2;
-
-%%%
-%thresh = 0.2;
-%%%
-syncwave = sign(syncwave-thresh);
-id = find(syncwave == 0);
-syncwave(id) = 1;
-
-syncwave = diff((syncwave+1)/2);
-
-synctimes = find(syncwave == 1) + 1;
-synctimes = synctimes/Fs;
-
-clear syncwave
-
+    syncwave = sign(syncwave - thresh);
+    syncwave(syncwave == 0) = 1;
+    syncwave = diff((syncwave + 1) / 2);
+    synctimes = find(syncwave == 1) + 1;
+    synctimes = synctimes / Fs;
+    clear syncwave
