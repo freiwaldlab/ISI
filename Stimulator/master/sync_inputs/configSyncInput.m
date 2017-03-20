047b4bfb1b00454e31bb0a95a7bbaed4102c8c9a
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-03-20 14:12:22 -0400

    added output handling for camera triggering

--- Stimulator/master/sync_inputs/configSyncInput.m
+++ Stimulator/master/sync_inputs/configSyncInput.m
@@ -1,24 +1,56 @@
 function configSyncInput
-    global analogIN
+    global analogIN daqOUTLED daqOUT2p daqOUTtrig daqOUTIOI
 
     % Check to be sure a DAQ board exists before trying to interact with it
-    have_ni = 0;
+    analogIN_devid = [];
+    analogIN_desc = 'USB-6000';
+    daqOUT_devid = [];
+    daqOUT_desc = 'USB-6001';
     devs = daq.getDevices;
     for dn = 1:length(devs)
-        if strcmp(devs(dn).Vendor.ID, 'ni')
-            have_ni = 1;
-            ni_dev_id = devs(dn).ID;
+        if strcmp(devs(dn).Vendor.ID, 'ni') && ...
+                ~isempty(strfind(devs(dn).Description, analogIN_desc))
+            analogIN_devid = devs(dn).ID;
+        end
+        if strcmp(devs(dn).Vendor.ID, 'ni') && ...
+                ~isempty(strfind(devs(dn).Description, daqOUT_desc))
+            daqOUT_devid = devs(dn).ID;
         end
     end
     % Establish connection with DAQ board
-    if have_ni
+    if ~isempty(analogIN_devid)
         analogIN = daq.createSession('ni');
-        addAnalogInputChannel(analogIN, ni_dev_id, 0:1, 'Voltage');
+        addAnalogInputChannel(analogIN, analogIN_devid, 0:4, 'Voltage');
+        %0: photodiode, 1: strobe, 2: trigger copy, 3: audio copy, 4:
+        %start/stop IOI ttl copy
         % Rate determined by maximum on device/number of channels
-        analogIN.Rate = 5000;
+        analogIN.Rate = 2000;
         analogIN.IsContinuous = true;
-        disp('configSyncInput: Configured DAQ analogIN.')
+        disp('configSyncInput: Configured DAQ analog input.')
     end
-    if ~have_ni || isempty(analogIN)
-        disp('configSyncInput ERROR: Could not find NI device.')
+    if ~isempty(daqOUT_devid)
+        daqOUTLED = daq.createSession('ni');
+        daqOUT2p = daq.createSession('ni');
+        daqOUTIOI = daq.createSession('ni');
+        daqOUTtrig = daq.createSession('ni');
+        % % % SET UP DAQ OUT
+        addDigitalChannel(daqOUTLED, daqOUT_devid, 'Port0/Line0', 'OutputOnly');
+        addDigitalChannel(daqOUT2p, daqOUT_devid, 'Port0/Line1', 'OutputOnly');
+        addDigitalChannel(daqOUTIOI, daqOUT_devid, 'Port0/Line2', 'OutputOnly'); %start/stop
+        addAnalogOutputChannel(daqOUTtrig, daqOUT_devid, 'ao1', 'Voltage');
+        % Initialize values
+        outputSingleScan(daqOUTLED, 0);
+        outputSingleScan(daqOUT2p, 0);
+        outputSingleScan(daqOUTIOI, 0);
+        daqOUTtrig.IsContinuous = true;
+        % %%% *** XXX TODO just putting in 1000 to make the trigger easy to construct in preallocate mmf
+        daqOUTtrig.Rate = 1000;
+        disp('configSyncInput: Configured DAQ output.')
+    end
+    if isempty(analogIN)
+        disp('configSyncInput ERROR: Problem configuring NI DAQ IN device.')
+    end
+    if isempty(daqOUTLED) || isempty(daqOUT2p) || isempty(daqOUTIOI) || ...
+            isempty(daqOUTtrig)
+        disp('configSyncInput ERROR: Problem configuring NI DAQ OUT device.')
     end
