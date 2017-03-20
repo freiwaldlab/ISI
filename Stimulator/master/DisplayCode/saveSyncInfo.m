db3b6c933c842a92142557d1ad7f06f33ed460b7
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-03-20 15:31:19 -0400

    removed debug output

--- Stimulator/master/DisplayCode/saveSyncInfo.m
+++ Stimulator/master/DisplayCode/saveSyncInfo.m
@@ -2,13 +2,10 @@ function saveSyncInfo(syncInfo)
     global Mstate trialno DataPath
 
     eval(['syncInfo' num2str(trialno) '=syncInfo;'])
-    %%clear syncInfo commented 170205
 
     % Make sure destination directory exists and save the analyzer file
-    title = [Mstate.anim '_' sprintf('u%s',Mstate.unit) '_' Mstate.expt];
+    title = [Mstate.anim '_' sprintf('u%s', Mstate.unit) '_' Mstate.expt];
     roots = parseString(DataPath, ';'); %parseString(Mstate.analyzerRoot,';');
-    disp(['saveSyncInfo DEBUG: Appending to analyzer file in main ' ... 
-        'experiment directory rather than analyzerRoot.'])
     for i = 1:length(roots)
         dd = [roots{i} filesep]; %[roots{i} filesep Mstate.anim];
         % Check if directory exists in case this is a new animal
