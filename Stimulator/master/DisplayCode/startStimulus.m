8e2cee73cebb3cbdbb16885946e9b30587701388
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-03-20 15:22:57 -0400

    commenting

--- Stimulator/master/DisplayCode/startStimulus.m
+++ Stimulator/master/DisplayCode/startStimulus.m
@@ -1,6 +1,7 @@
 function startStimulus
+% Send start command to slave and indicate module ID
     global DcomState
-    % Send start command to slave and indicate module ID
+    
     mod = getmoduleID;
     msg = ['G;' mod ';~'];
     fwrite(DcomState.serialPortHandle, msg);
