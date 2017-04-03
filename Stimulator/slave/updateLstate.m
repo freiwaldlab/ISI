2c18f5024e1b3886fd8650d30b20be1ea23f4cd3
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-04-03 10:33:41 -0400

    cleanup and readability

--- Stimulator/slave/updateLstate.m
+++ Stimulator/slave/updateLstate.m
@@ -1,19 +1,12 @@
 function updateLstate
     global Lstate GUIhandles
-    Gh = GUIhandles;
-    
-    Lstate.reps = str2double(get(Gh.looper.repeats, 'string'));
-    Lstate.rand = get(Gh.looper.randomflag, 'value');
 
-    Lstate.param{1} = {[get(Gh.looper.symbol1, 'string')] ...
-        [get(Gh.looper.valvec1, 'string')]};
-    Lstate.param{2} = {[get(Gh.looper.symbol2, 'string')] ...
-        [get(Gh.looper.valvec2, 'string')]};
-    Lstate.param{3} = {[get(Gh.looper.symbol3, 'string')] ...
-        [get(Gh.looper.valvec3, 'string')]};
-    %Lstate.param{4} = {[get(Gh.looper.symbol4, 'string')] ... 
-    %    [get(Gh.looper.valvec4, 'string')]};
-    %Lstate.param{5} = {[get(Gh.looper.symbol5, 'string')] ... 
-    %    [get(Gh.looper.valvec5, 'string')]};
-    
-    GUIhandles = Gh;
+    Lstate.reps = str2double(get(GUIhandles.looper.repeats, 'string'));
+    Lstate.rand = get(GUIhandles.looper.randomflag, 'value');
+
+    Lstate.param{1} = {[get(GUIhandles.looper.symbol1, 'string')] ...
+        [get(GUIhandles.looper.valvec1, 'string')]};
+    Lstate.param{2} = {[get(GUIhandles.looper.symbol2, 'string')] ...
+        [get(GUIhandles.looper.valvec2, 'string')]};
+    Lstate.param{3} = {[get(GUIhandles.looper.symbol3, 'string')] ...
+        [get(GUIhandles.looper.valvec3, 'string')]};
