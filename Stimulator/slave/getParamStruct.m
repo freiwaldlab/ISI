479447b914a8597449333099467d5d3c38655aac
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-04-03 10:32:43 -0400

    cleanup and readability

--- Stimulator/slave/getParamStruct.m
+++ Stimulator/slave/getParamStruct.m
@@ -1,6 +1,6 @@
-function PStruct = getParamStruct
+function P = getParamStruct
     global Pstate
 
     for i = 1:length(Pstate.param)
-        eval(['PStruct.' Pstate.param{i}{1} '= Pstate.param{i}{3} ;'])
+        eval(['P.' Pstate.param{i}{1} '= Pstate.param{i}{3} ;'])
     end
--- Stimulator/slave/makeTextures/makeBar.m
+++ Stimulator/slave/makeTextures/makeBar.m
@@ -1,27 +1,29 @@
-function barout = makeBar(Wx,Wy,ori)
+function barout = makeBar(Wpx, Hpx, ori)
+% Wpx,Hpx are size in pixels uncorrected for different x,y resolutions
+    domy = linspace(0, 2 * max([Wpx Hpx]), round(2 * max([Wpx Hpx])));
+    domx = linspace(0, 2 * max([Wpx Hpx]), round(2 * max([Wpx Hpx])));
+    domx = domx - median(domx);
+    domy = domy - median(domy);
 
-%Wx,Wy are the size of the bar in pixels (hasn't yet corrected for different x,y resolutions)
+    [domx, domy] = meshgrid(domx, domy);
 
-domy = linspace(0,2*max([Wx Wy]),round(2*max([Wx Wy])));
-domx = linspace(0,2*max([Wx Wy]),round(2*max([Wx Wy])));
-domx = domx-median(domx);
-domy = domy-median(domy);
+    xp = domx * cos(-ori * pi/180) + domy * sin(-ori * pi/180);
+    yp = domx * sin(-ori * pi/180) - domy * cos(-ori * pi/180);
 
-[domx domy] = meshgrid(domx,domy);
+    idx = find((xp <= Wpx/2) && (xp >= -Wpx/2) && ...
+        (yp <= Hpx/2) && (yp >= -Hpx/2));
+    temp = zeros(size(domx));
+    temp(idx) = 1;
 
-xp = domx*cos(-ori*pi/180) + domy*sin(-ori*pi/180);
-yp = domx*sin(-ori*pi/180) - domy*cos(-ori*pi/180);
+    [domx, domy] = meshgrid(1:length(domx(1,:)), 1:length(domx(:,1)));
 
-idx = find(xp <= Wx/2 & xp >= -Wx/2  &  yp <= Wy/2 & yp >= -Wy/2);
-
-temp = zeros(size(domx));
-temp(idx) = 1;
-
-[domx domy] = meshgrid(1:length(domx(1,:)),1:length(domx(:,1)));
-
-idxmin = min(domx(idx)); idxmin = idxmin(1);
-idxmax = max(domx(idx)); idxmax = idxmax(1);
-idymin = min(domy(idx)); idymin = idymin(1);
-idymax = max(domy(idx)); idymax = idymax(1);
-
-barout = temp(idymin:idymax,idxmin:idxmax);
+    idxmin = min(domx(idx));
+    idxmin = idxmin(1);
+    idxmax = max(domx(idx));
+    idxmax = idxmax(1);
+    idymin = min(domy(idx));
+    idymin = idymin(1);
+    idymax = max(domy(idx));
+    idymax = idymax(1);
+    
+    barout = temp(idymin:idymax, idxmin:idxmax);
