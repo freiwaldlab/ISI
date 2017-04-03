85db8ae332266994216f0b5e9c4d94f4752dc027
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-04-03 10:29:30 -0400

    save default changed to compressionless -v6 for speed

--- Stimulator/slave/saveLog_new.m
+++ Stimulator/slave/saveLog_new.m
@@ -1,35 +1,18 @@
-function saveLog_new(domains,seqs)
-
-
+function saveLog_new(domains, seqs)
 global Mstate loopTrial
 
-root = '/Matlab_code/log_files/';
-
-rootnet = ['/Volumes/neurostuff/log_files/' Mstate.anim '/'];
-
+root = 'C:\Dropbox\ExperimentLogs\';
 expt = [Mstate.anim '_' Mstate.unit '_' Mstate.expt];
-
 fname = [root expt '.mat'];
-fnamenet = [rootnet expt '.mat'];
-
 frate = Mstate.refresh_rate;
-
-%%%
-
 basename = ['randlog_T' num2str(loopTrial)];
 
 eval([basename '.seqs = seqs;'])
 eval([basename '.domains = domains;'])
 
 if loopTrial == 1
-    save(fname,['randlog_T' num2str(loopTrial)])
-    save(fnamenet,['randlog_T' num2str(loopTrial)])
-    
-    save(fname,'frate','-append')
-    save(fnamenet,'frate','-append')
+    save(fname, ['randlog_T' num2str(loopTrial)], '-v6');
+    save(fname,'frate', '-v6', '-append');
 else
-    save(fname,['randlog_T' num2str(loopTrial)],'-append')
-    save(fnamenet,['randlog_T' num2str(loopTrial)],'-append')
+    save(fname, ['randlog_T' num2str(loopTrial)], '-v6', '-append');
 end
-
-
--- Stimulator/slave/saveLog_rain.m
+++ Stimulator/slave/saveLog_rain.m
@@ -1,30 +1,17 @@
-function saveLog_rain(x,varargin)
-
+function saveLog_rain(x, varargin)
 global Mstate
 
-root = '/Matlab_code/log_files/';
-
-rootnet = ['/Volumes/neurostuff/log_files/' Mstate.anim '/'];
-
+root = 'C:\Dropbox\ExperimentLogs\';
 expt = [Mstate.anim '_' Mstate.unit '_' Mstate.expt];
-
 fname = [root expt '.mat'];
-fnamenet = [rootnet expt '.mat'];
-
 frate = Mstate.refresh_rate;
 
-if isempty(varargin)  %from 'make'  (happens on first trial only)
-
+if isempty(varargin)  %from 'make' (happens on first trial only)
     domains = x; 
     if ~exist(fname)
-        save(fname,'domains','frate')    
-        save(fnamenet,'domains','frate')
+        save(fname, 'domains', 'frate', '-v6');
     end
-    
 else %from 'play'
-    
-    eval(['rseed' num2str(varargin{1}) '=x;' ])
-    eval(['save ' fname ' rseed' num2str(varargin{1}) ' -append'])    
-    eval(['save ' fnamenet ' rseed' num2str(varargin{1}) ' -append'])   
-    
+    eval(['rseed' num2str(varargin{1}) '=x;' ]);
+    eval(['save ' fname ' rseed' num2str(varargin{1}) ' -v6 -append']);
 end
