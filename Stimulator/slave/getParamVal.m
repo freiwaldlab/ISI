da1b7abbd6071950b051570d5586e874b0a82559
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-04-03 10:42:11 -0400

    cleanup and readability

--- Stimulator/slave/getParamVal.m
+++ Stimulator/slave/getParamVal.m
@@ -1,12 +1,10 @@
 function pval = getParamVal(psymbol)
+    global Pstate
 
-global Pstate
-
-for i = 1:length(Pstate.param)
-    if strcmp(psymbol,Pstate.param{i}{1})
-    	idx = i;
-        break;
+    for i = 1:length(Pstate.param)
+        if strcmp(psymbol, Pstate.param{i}{1})
+        	idx = i;
+            break;
+        end
     end
-end
-
-pval = Pstate.param{idx}{3};
+    pval = Pstate.param{idx}{3};
