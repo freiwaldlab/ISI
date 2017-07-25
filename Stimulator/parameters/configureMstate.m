55be15b195192ea29325478dda74444cc4ab39a3
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-07-25 17:01:28 -0400

    changed default distance, analyzer root

--- Stimulator/parameters/configureMstate.m
+++ Stimulator/parameters/configureMstate.m
@@ -9,12 +9,12 @@ function configureMstate
     % This should match the default value in Display
     Mstate.monitor = 'VPX';
     % Set distance of animal from screen (in cm)
-    Mstate.screenDist = 25;
+    Mstate.screenDist = 57;
     % Set size of the screen sync (in cm)
     Mstate.syncSize = 2.0;
 
     % Set path for saving experimental parameters
-    Mstate.analyzerRoot = 'C:\Data\AnalyzerFiles';
+    Mstate.analyzerRoot = 'D:\';
 
     % Set slave computer IP for communication
     Mstate.stimulusIDP = '192.168.1.101'; %'129.85.181.189';
