894636dce36d49bb66852f778488fde597836777
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-08-22 20:33:42 -0400

    spherical bar stimulus cleanup; readability

--- Stimulator/slave/makeTextures/makeSphericalBarTexture.m
+++ Stimulator/slave/makeTextures/makeSphericalBarTexture.m
@@ -2,176 +2,149 @@ function makeSphericalBarTexture
 % original author: Onyekachi 'Kachi' Odoemene 2016-06-30
 %drifting bar stimulus i.e. full field drifting grating
     global Mstate screenPTR screenNum
-    global srcRect destRect 
-    global SyncStateTxtr barTex
-    global syncWX syncWY SyncLoc SyncPiece
+    global barTex srcRect dstRect
     P = getParamStruct;
     window = screenPTR;
 
     screenRes = Screen('Resolution', screenNum);
     resXpxpercm = screenRes.width / Mstate.screenXcm;
     resYpxpercm = screenRes.height / Mstate.screenYcm;
-    syncWpx = round(resXpxpercm * Mstate.syncSize);
-    syncHpx = round(resYpxpercm * Mstate.syncSize);
     
     white = WhiteIndex(window);
     black = BlackIndex(window);
     grey = (white + black) / 2;
     inc = white - grey;
     
-Screen('FillRect', screenPTR, black);
-Screen('Flip', screenPTR);
+    barThickDeg = P.BarThickness;
+    checkXYdeg = P.CheckSize;
+    flick_rate = P.FlickerRate;
+    bar_ori = P.BarOrient;
 
-barThicknessDeg = P.BarThickness;	
-barChecksize = P.CheckSize; %in degrees, if 0 make white bar
-flickerBar = P.FlickerRate;
-barOrient = P.BarOrient;
-%numcycles = P.NumCycles;
-%stimdur = P.stim_time;
-%movieDurationSecs = stimdur/numcycles; % Run the movie animation for a fixed period.
-
-screenDistance = Mstate.screenDist; %in cm, needs to be converted to pixels
-w = Mstate.screenXcm; %width of monitor
-h = Mstate.screenYcm; %height of monitor
-cx = P.eyeXLocation; %eye x location, cm
-cy = P.eyeYLocation; %eye y location, cm
-
-zdistTop = hypot(screenDistance,h-cy); %distance from eye to top of screen
-zdistBottom = hypot(screenDistance,cy); %distance from eye to bottom of screen
-
-%priorityLevel = MaxPriority(screenPTR); %#ok<NASGU>
-
-scale = P.ScreenScaleFactor;
-pxXmax = screenRes.width / scale;
-pxYmax = screenRes.height / scale;
-resXpxpercm = round(pxXmax / w);
-resYpxpercm = round(pxYmax / h);
-
-%internal conversions?
-top = h - cy;
-bottom = -cy;
-right = cx;
-left = cx - w;
-
-[xi,yi] = meshgrid(1:pxXmax,1:pxYmax);
-cart_pointsX = left + (w/pxXmax).*xi;
-cart_pointsY = top - (h/pxYmax).*yi;
-cart_pointsZ = zdistTop + ((zdistBottom - zdistTop)/pxYmax).*yi;
-[sphr_pointsTh, sphr_pointsPh, sphr_pointsR] = cart2sph(cart_pointsZ,cart_pointsX,cart_pointsY);
-
-%rescale Cartesian maps into dimensions of radians
-xmaxRad = max(sphr_pointsTh(:));
-ymaxRad = max(sphr_pointsPh(:));
-fx = xmaxRad/max(cart_pointsX(:));
-fy = ymaxRad/max(cart_pointsY(:));
-
-%% determine stimulus size in pixels
-% barThicknessPixels = round(tan(barThicknessDeg)*screenDistance
-
-texsizeY = ceil(pxXmax/2);
-texsizeZ = ceil(pxYmax/2);
-
-% This is the visible size of the texture. It is twice the half-width
-% of the texture plus one pixel to make sure it has an odd number of
-% pixels and is therefore symmetric around the center of the texture:
-visiblesizeX = 2*texsizeY+1;
-visiblesizeY = 2*texsizeZ+1;
-
-%% define destination of stimuli
-frameRate = Screen('FrameRate', screenNum);
-if frameRate == 0
-    frameRate = 60;
-end
-Mstate.screenFrameRate = frameRate;
-numframes = floor(frameRate); %temporal period, i.e. number of frames in one cycle of bar sweep
-
-barThicknesscm = tan(barThicknessDeg * pi/180) * screenDistance;
-barXThickness = round(barThicknesscm * resXpxpercm); %vertical bar
-barYThickness = round(barThicknesscm * resYpxpercm); %horizontal bar
-
-screenSize = [pxYmax pxXmax];
-if barOrient
-% horizontal bar
-    barAngle = 90;
-    barThickness = barYThickness;
-    thisScreenSize = screenSize(1) + 2*barThickness; %direction for horizontal bar, vertical travel
-else
-% vertical bar
-    barAngle = 0;
-    barThickness = barXThickness;
-    thisScreenSize = screenSize(2) + 2*barThickness; %azimuth travel
-end
-cyclesPerPixel = 1/(thisScreenSize);
-dutyCycle = 1 - (barThickness/thisScreenSize); %determines bar thickness
-
-% grating
-[x,y] = meshgrid(-screenSize(2)/2:screenSize(2)/2-1, ...
-    -screenSize(1)/2:screenSize(1)/2-1);
-
-angle = barAngle*pi/180; % bar orientation.
-f = cyclesPerPixel*2*pi; % cycles/pixel
-
-a = cos(angle)*f;
-b = sin(angle)*f;
-
-flickerInds = ones(1, numframes); %non-flickering checkerboard
-barBackground = ones(pxYmax, pxXmax, numframes);
-% barBackground = ones(pxYmax,pxXmax,flickerInds); % throws error
-
-if barChecksize > 0
-    barChecksizecm = tan(barChecksize*pi/180)*screenDistance;
-    barChecksizepixels = round(barChecksizecm*resYpxpercm);
-
-    checkerPattern = double(checkerboard(barChecksizepixels,1+round((pxYmax/2)/barChecksizepixels),1+round((pxXmax/2)/barChecksizepixels))<0.5);
-    checkerPattern = checkerPattern(1:pxYmax,1:pxXmax);
-    barBackground(:,:,1) = checkerPattern;  %checkerboard
-    barBackground(:,:,2) = abs(1-checkerPattern); %reverse checkerboard
+    screenDcm = Mstate.screenDist; %in cm, needs to be converted to pixels
+    w = Mstate.screenXcm; %width of monitor
+    h = Mstate.screenYcm; %height of monitor
+    cx = P.eyeXLocation; %eye x location, cm
+    cy = P.eyeYLocation; %eye y location, cm
     
-    if flickerBar >0
-        flipChecksFrames = (frameRate/flickerBar)/2; %convert to frames per cycle
-        flickerInds = floor(mod((0:numframes)/flipChecksFrames,2))+1;
+    zdistTop = hypot(screenDcm, h - cy); %distance from eye to top of screen
+    zdistBottom = hypot(screenDcm, cy); %distance from eye to bottom of screen
+    
+    scale = P.ScreenScaleFactor;
+    pxXmax = screenRes.width / scale;
+    pxYmax = screenRes.height / scale;
+    resXpxpercm = round(pxXmax / w);
+    resYpxpercm = round(pxYmax / h);
+    
+    %internal conversions?
+    top = h - cy;
+    bottom = -cy;
+    right = cx;
+    left = cx - w;
+    
+    [xi,yi] = meshgrid(1:pxXmax,1:pxYmax);
+    cart_pointsX = left + (w/pxXmax).*xi;
+    cart_pointsY = top - (h/pxYmax).*yi;
+    cart_pointsZ = zdistTop + ((zdistBottom - zdistTop)/pxYmax).*yi;
+    [sphr_pointsTh, sphr_pointsPh, ~] = cart2sph(cart_pointsZ,cart_pointsX,cart_pointsY);
+    
+    %rescale Cartesian maps into dimensions of radians
+    xmaxRad = max(sphr_pointsTh(:));
+    ymaxRad = max(sphr_pointsPh(:));
+    fx = xmaxRad / max(cart_pointsX(:));
+    fy = ymaxRad / max(cart_pointsY(:));
+    
+    % determine stimulus size in pixels
+    % barThicknessPixels = round(tan(barThicknessDeg)*screenDistance
+    texsizeY = ceil(pxXmax / 2);
+    texsizeZ = ceil(pxYmax / 2);
+    
+    % This is the visible size of the texture. It is twice the half-width
+    % of the texture plus one pixel to make sure it has an odd number of
+    % pixels and is therefore symmetric around the center of the texture:
+    visiblesizeX = 2*texsizeY + 1;
+    visiblesizeY = 2*texsizeZ + 1;
+    
+    %% define destination of stimuli
+    frameRate = Screen('FrameRate', screenNum);
+    if frameRate == 0
+        warning([mfilename ': FrameRate for screen returned 0Hz.  Assuming 60Hz.']);
+        frameRate = 60;
+    end
+    Mstate.screenFrameRate = frameRate;
+    %temporal period, i.e. number of frames in one cycle of bar sweep
+    frameN = floor(frameRate);
+    barThicknesscm = tan(barThickDeg * pi/180) * screenDcm;
+    barXThickness = round(barThicknesscm * resXpxpercm); %vertical bar
+    barYThickness = round(barThicknesscm * resYpxpercm); %horizontal bar
+    
+    if bar_ori
+        % horizontal bar, altitude travel
+        barAngle = 90;
+        barThickness = barYThickness;
+        thisScreenSize = pxYmax + 2*barThickness;
+    else
+        % vertical bar, azimuth travel
+        barAngle = 0;
+        barThickness = barXThickness;
+        thisScreenSize = pxXmax + 2*barThickness;
+    end
+    cyclesPerPixel = 1 / thisScreenSize;
+    dutyCycle = 1 - (barThickness / thisScreenSize); %determines bar thickness
+    
+    % Generate grating
+    [x,y] = meshgrid(-pxXmax/2:pxXmax/2-1, -pxYmax/2:pxYmax/2-1);
+    
+    angle = barAngle * pi/180;
+    f = cyclesPerPixel * 2*pi; % cycles/pixel
+    
+    a = f*cos(angle);
+    b = f*sin(angle);
+    
+    %flipInds = ones(1, frameN); % start with non-flickering checkerboard
+    %bars = ones(pxYmax, pxXmax, 2);
+    chkbar = ones(pxYmax, pxXmax);
+    if checkXYdeg > 0
+        checkXYcm = screenDcm * tan(checkXYdeg * pi/180);
+        checkXYpx = round(checkXYcm * resYpxpercm);
+        checkPtrn = double(checkerboard(checkXYpx, ...
+            1+round((pxYmax/2)/checkXYpx), ...
+            1+round((pxXmax/2)/checkXYpx))<0.5);
+        checkPtrn = checkPtrn(1:pxYmax,1:pxXmax);
+        chkbar = checkPtrn;
+        %chkbar(:,:,1) = checkPtrn;
+        %chkbar(:,:,2) = abs(1 - checkPtrn);
+        %if flick_rate > 0
+        %    %convert to frames per cycle
+        %    flipFrames = (frameRate / flick_rate) / 2;
+        %    flipInds = floor(mod((0:frameN) / flipFrames, 2)) + 1;
+        %end
     end
     
-end
-
-% Compute each frame of the movie and convert the frames, stored in
-% MATLAB matices, into Psychtoolbox OpenGL textures using 'MakeTexture';
-barTex = nan(1,numframes); 
-%
-
-timestart = tic;
-
-for ff = 1:numframes
-    phase=((ff-1)/(numframes-1))*2*pi; %controls movement
-    img=cos(a*x+b*y+phase);   %from DriftDemo2.m and Ian Nauhaus' code %makePerGratFrame_insep.m
-    img = sign(img - cos(dutyCycle*pi)); %from Ian Nauhaus' code %makePerGratFrame_insep.m
-    img = double(img<0);
-    
-    thisBarBackground = barBackground(:,:,flickerInds(ff));
-    
-    barTextureImg = img .* thisBarBackground;
-
-    if P.sphereCorrectON
-        barTextureImg = interp2(cart_pointsX.*fx,cart_pointsY.*fy,barTextureImg,sphr_pointsTh,sphr_pointsPh);
+    barTex = nan(2, frameN);
+    for f = 1:frameN
+        %controls movement
+        phase = ((f-1) / (frameN-1))*2*pi;
+        %from DriftDemo2.m and Ian Nauhaus' code makePerGratFrame_insep.m
+        img = cos(a*x + b*y + phase);
+        img = sign(img - cos(dutyCycle*pi));
+        img = double(img < 0);
+        
+        thisBar = chkbar;
+        flipBar = abs(1 - chkbar);
+        %thisBar = chkbar(:,:,flipInds(f));
+        thisImg = img .* thisBar;
+        flipImg = img .* flipBar;
+        if P.sphereCorrectON
+            thisImg = interp2(cart_pointsX .* fx, cart_pointsY .* fy, ...
+                thisImg, sphr_pointsTh, sphr_pointsPh);
+            flipImg = interp2(cart_pointsX .* fx, cart_pointsY .* fy, ...
+                flipImg, sphr_pointsTh, sphr_pointsPh);
+        end
+        thisImg = white * thisImg;
+        flipImg = white * flipImg;
+        barTex(1,f) = Screen('MakeTexture', screenPTR, thisImg);
+        barTex(2,f) = Screen('MakeTexture', screenPTR, flipImg);
     end
     
-    barTextureImg = white*(barTextureImg);
-    barTex(1,ff)=Screen('MakeTexture', screenPTR, barTextureImg);
-    
-end
-
-disp([mfilename ': Done making texture.']);
-toc(timestart)
-
-srcRect=[0 0 visiblesizeX visiblesizeY]; %texture size
-destRect = [0 0 pxXmax*scale pxYmax*scale]; %scale the stimulus to fill the screen
-
-%
-syncWX = round(resXpxpercm*Mstate.syncSize*scale);
-syncWY = round(resYpxpercm*Mstate.syncSize*scale);
-
-SyncStateTxtr(1) = Screen(screenPTR, 'MakeTexture', white*ones(syncWY,syncWX)); % "hi"
-SyncStateTxtr(2) = Screen(screenPTR, 'MakeTexture', black*ones(syncWY,syncWX)); % "low"
-% coordinates for sync marker
-SyncLoc = [0 0 syncWX-1 syncWY-1]';
-SyncPiece = [0 0 syncWX-1 syncWY-1]';
+    srcRect = [0 0 visiblesizeX visiblesizeY]';  % texture size
+    dstRect = [0 0 pxXmax*scale pxYmax*scale]';  % scale to screen
--- Stimulator/slave/playtextures/playSphericalBar.m
+++ Stimulator/slave/playtextures/playSphericalBar.m
@@ -1,100 +1,99 @@
 function playSphericalBar
-% original author Onyekachi 'Kachi' Odoemene 30June2016
-global screenPTR Mstate srcRect destRect 
-global SyncStateTxtr barTex  SyncLoc SyncPiece
-P = getParamStruct;
+    % original author Onyekachi 'Kachi' Odoemene 2016-06-30
+    global Mstate screenPTR screenNum
+    global barTex srcRect dstRect  % from makeSphericalBarTexture
+    global Stxtr  % from makeSyncTexture
+    syncHigh = Stxtr(1);
+    syncLow = Stxtr(2);
+    P = getParamStruct;
+    window = screenPTR;
+    
+    screenRes = Screen('Resolution', screenNum);
+    resXpxpercm = screenRes.width / Mstate.screenXcm;
+    resYpxpercm = screenRes.height / Mstate.screenYcm;
+    syncWpx = round(resXpxpercm * Mstate.syncSize);
+    syncHpx = round(resYpxpercm * Mstate.syncSize);
+    ifi = Screen('GetFlipInterval', window);
+    white = WhiteIndex(window);
+    black = BlackIndex(window);
+    grey = (white + black) / 2;
+    inc = white - grey;
 
-black = BlackIndex(screenPTR);
-Screen('FillRect', screenPTR, black);
-Screen('Flip', screenPTR);
-
-% flickerBar = Pstruct.FlickerRate * (Pstruct.CheckSize ~=0);
-direction = P.BarDirection;
-numcycles = P.NumCycles;
-
-stimdur = P.stim_time;
-movieDurationSecs = stimdur/numcycles; % Run the movie animation for a fixed period.
-
-frameRate  = Mstate.screenFrameRate;
-numframes = floor(frameRate); %temporal period, i.e. number of frames in one cycle of bar sweep
-
-% Convert movieDuration in seconds to duration in frames to draw:
-movieDurationFrames=round(movieDurationSecs * frameRate);
-movieFrameIndices=floor(mod((0:movieDurationFrames-1)/(movieDurationFrames/numframes), numframes)) + 1;
-
-if direction ==1 %forward direction
-    movieFrameIndices = reshape(movieFrameIndices,1,max(size(movieFrameIndices)));
-    movieFrameIndices = fliplr(movieFrameIndices);
-   
-end
-
-% 
-% if flickerBar
-%     flipBarframes = (frameRate/flickerBar)/2; %convert to frames per cycle
-%     flipBar = floor(mod((0:movieDurationFrames)/flipBarframes,2))+1;    
-% else
-    flipBar = ones(1,movieDurationFrames);
-
-% end
-
-% Use realtime priority for better timing precision:
-priorityLevel=MaxPriority(screenPTR);
-Priority(priorityLevel);
-
- % Query duration of one monitor refresh interval:
-ifi=Screen('GetFlipInterval', screenPTR);
-
-% Translate that into the amount of seconds to wait between screen
-% redraws/updates:
-
-% waitframes = 1 means: Redraw every monitor refresh. 
-waitframes = 1;       
-
-%% prestimulus delay
-Screen('DrawTexture', screenPTR, SyncStateTxtr(2),SyncPiece,SyncLoc);
-prestimTimeR  = Screen(screenPTR, 'Flip');% Sync us to the vertical retrace
-PreStimTime  = prestimTimeR + P.predelay;
-
-while prestimTimeR < PreStimTime
-    Screen('DrawTexture', screenPTR, SyncStateTxtr(1),SyncPiece,SyncLoc);
-    prestimTimeR = Screen('Flip', screenPTR, prestimTimeR + (waitframes - 0.5) * ifi);
-end
-Screen('DrawTexture', screenPTR, SyncStateTxtr(2),SyncPiece,SyncLoc);
-Screen(screenPTR, 'Flip');
-
-tic        
-%% present Stimulus animation loop:
-vbl = Screen('Flip',screenPTR);
-
-for n = 1:numcycles
-    for i=1:movieDurationFrames
-        % Draw image:
-        Screen('DrawTextures', screenPTR, [SyncStateTxtr(2) barTex(flipBar(i),movieFrameIndices(i))],[SyncPiece srcRect],[SyncLoc destRect]);
-%         Screen('Flip', screenPTR);
-        Screen('DrawingFinished',screenPTR);
-        vbl = Screen('Flip',screenPTR,vbl+(waitframes-0.5)*ifi);
+    syncPos = [0 0 (syncWpx - 1) (syncHpx - 1)]';
+    syncPiece = [0 0 (syncWpx - 1) (syncHpx - 1)]';
+    
+    % Set screen to black background before pre-delay
+    Screen('FillRect', window, black);
+    Screen('Flip', window);
+    
+    if P.CheckSize ~= 0
+        flipRate = P.FlickerRate;
+    else
+        flipRate = 0;
     end
-end
-toc
+    direction = P.BarDirection;
+    cycleN = P.NumCycles;
+    
+    % Run the movie animation for a fixed period.
+    stimTsec = P.stim_time;
+    movTsec = stimTsec / cycleN;
+    
+    frameRate = Screen('FrameRate', screenNum);
+    %temporal period, i.e. number of frames in one cycle of bar sweep
+    frameN = floor(frameRate);
+    
+    % Convert movieDuration in seconds to duration in frames to draw:
+    movFrames = round(movTsec * frameRate);
+    movFrameInds = floor(mod((0:movFrames-1)/(movFrames/frameN), frameN)) + 1;
+    
+    if direction == 1 %forward direction
+        movFrameInds = reshape(movFrameInds, 1, max(size(movFrameInds)));
+        movFrameInds = fliplr(movFrameInds);
+    end
+    
+    if flipRate
+        flipFrames = (frameRate / flipRate) / 2; %convert to frames per cycle
+        flipBar = floor(mod((0:movFrames)/flipFrames, 2)) + 1;
+    else
+        flipBar = ones(1, movFrames);
+    end
+    
+    % Translate that into the amount of seconds to wait between screen
+    % redraws/updates:
+    
+    % waitframes = 1 means: Redraw every monitor refresh.
+    waitframes = 1;
+    
+    % Pre-delay
+    % Draw "high" sync state for duration of pre-delay block
+    Screen('DrawTexture', window, syncLow, syncPiece, syncPos);
+    Screen('Flip', window);
+    Screen('DrawTexture', window, syncHigh, syncPiece, syncPos);
+    Screen('Flip', window);
+    WaitSecs(P.predelay);
+    Screen('DrawTexture', window, syncLow, syncPiece, syncPos);
+    Screen('Flip', window);
+    
+    % Play stimulus by drawing pre-generated textures to the screen
+    vbl = Screen('Flip', window);
+    for n = 1:cycleN
+        for i = 1:movFrames
+            Screen('DrawTextures', window, ...
+                [barTex(flipBar(i), movFrameInds(i)) syncLow], ...
+                [srcRect syncPiece], [dstRect syncPos]);
+            Screen('DrawingFinished', window);
+            vbl = Screen('Flip', window, vbl + (waitframes - 0.5) * ifi);
+        end
+    end
+    Screen('FillRect', window, black);
+    Screen('Flip', window);
 
-Screen('FillRect', screenPTR,black);
-Screen('Flip', screenPTR);
-
-%% Poststimulus loop--------------------------------%%%%%%%%%%%%:
-
-Screen('DrawTexture', screenPTR, SyncStateTxtr(2),SyncPiece,SyncLoc);
-postStimTimeR  = Screen(screenPTR, 'Flip');
-PostStimTime  = postStimTimeR + P.postdelay;
-
-while postStimTimeR < PostStimTime
-    Screen('DrawTexture', screenPTR, SyncStateTxtr(1),SyncPiece,SyncLoc);
-    postStimTimeR = Screen('Flip', screenPTR, postStimTimeR + (waitframes - 0.5) * ifi);
-end
-
-Screen(screenPTR, 'Flip');
-Screen('DrawTexture', screenPTR, SyncStateTxtr(2),SyncPiece,SyncLoc);
-Screen(screenPTR, 'Flip');
-
-Priority(0);
-
-return
+    % Post-delay
+    % Draw "high" sync state for duration of post-delay block
+    Screen('DrawTexture', window, syncLow, syncPiece, syncPos);
+    Screen('Flip', window);
+    Screen('DrawTexture', window, syncHigh, syncPiece, syncPos);
+    Screen('Flip', window);
+    WaitSecs(P.postdelay);
+    Screen('DrawTexture', window, syncLow, syncPiece, syncPos);
+    Screen('Flip', window);
