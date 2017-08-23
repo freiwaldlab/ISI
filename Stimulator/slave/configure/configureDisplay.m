20b4172e04703bf527e031ca72540b8927cebc3b
Freiwald Marmosets <freiwald.marmosets@gmail.com>
2017-08-22 20:28:48 -0400

    dynamic priority setting

--- Stimulator/slave/configure/configureDisplay.m
+++ Stimulator/slave/configure/configureDisplay.m
@@ -1,13 +1,16 @@
 function configureDisplay(varargin)
+    global screenPTR
     close all
+    window = screenPTR;
 
+    max_priority = MaxPriority(window);
     % %Make sure priority is set to "real-time"  
     %Priority(5);
-    % Set priority to 2 for now, because it appears to work
-    Priority(2);
-
-    %priorityLevel=MaxPriority(w);
-    %Priority(priorityLevel);
+    % Set priority to 2, which appears to work
+    %Priority(2);
+    % Set maximum priority 
+    Priority(max_priority);
+    disp([mfilename ': screen priority set to ' num2str(max_priority) '.']);
 
     % Initialize stimulus parameter structures,
     % defaulting to Image Block mode
